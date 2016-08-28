71d313eb8f7d026d67e9202ab2a2d5fd11c3e778
Pisei <limpisei@gmail.com>
2016-08-28 14:11:34 -0700

    init commit

---
+++ app.js
@@ -0,0 +1,11 @@
+for (i = 1; i <= 15; i++ ) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    $('body').append("<p>FizzBuzz</p>");
+  } else if (i % 5 == 0) {
+    $('body').append("<p>Buzz</p>");
+  } else if ( i % 3 == 0) {
+    $('body').append("<p>Fizz</p>");
+  } else {
+    $('body').append("<p>" + i + "</p>");
+  }
+}
